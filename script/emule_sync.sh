f8f7812e327e22ff21540c7b5698f153930954e3
Zheng Kai <zhengkai@gmail.com>
2011-09-07 17:51:02 +0800

    fix

--- script/emule_sync.sh
+++ script/emule_sync.sh
@@ -17,7 +17,7 @@ echo `date +'%Y-%m-%d %H:%M:%S'` > /tmp/emule_rsync_run
 #/usr/bin/rsync --temp-dir=/www/tmp --partial -vzrtopg -e "ssh -o TCPKeepAlive=yes -o ServerAliveInterval=30" aeon:/var/lib/mldonkey/incoming/files/ /www/emule/
 #/usr/bin/rsync --temp-dir=/www/tmp --partial -vzrtopg -e "ssh -o TCPKeepAlive=yes -o ServerAliveInterval=30" aeon:/home/zhengkai/rtorrent/download/ /www/emule/
 
-/usr/bin/rsync --temp-dir=/www/tmp --partial -vzrtopg -e "ssh -o TCPKeepAlive=yes -o ServerAliveInterval=30" hades:/var/www/soulogic.com/iso/ /www/iso/
+/usr/bin/rsync --temp-dir=/share/tmp --partial -vzrtopg -e ssh garona:~/rtorrent/move/ /share/garona/
 
 #/usr/bin/rsync --include "apache-log**" --include "awstats**" --include "bin**" --include "chroot**" --include "etc**" --include "home**" --include "ifstat**" --include "lib**" --include "mnt**" --include "opt**" --include "root**" --include "rsync**" --include "sbin**" --include "svn**" --include "usr**" --include "var**" --include "www**" --exclude "*" --temp-dir=/www/tmp --partial -vzrtopg -e "ssh -o TCPKeepAlive=yes -o ServerAliveInterval=30 -l root" aeon:/ /www/aeon/
 
