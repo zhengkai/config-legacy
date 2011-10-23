87b28d165236296eaf41fae514a774c157b87bf3
Zheng Kai <zhengkai@gmail.com>
2011-10-24 01:20:01 +0800

    auto save

--- script/shiva_vim_ctags.sh
+++ script/shiva_vim_ctags.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 scandir="/www/shiva"
-ctags --languages=-JavaScript --tag-relative=yes -f ~/tags -R $scandir
+ctags --languages=-JavaScript -a --extra=+q --tag-relative=yes -f ~/tags -R $scandir
 
