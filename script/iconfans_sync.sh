9704b01539ed690142a296efdbced86d4562e75d
Zheng Kai <zhengkai@gmail.com>
2011-11-03 12:40:06 +0800

    auto save from Freya

--- script/iconfans_sync.sh
+++ script/iconfans_sync.sh
@@ -3,8 +3,8 @@ run_file="/tmp/iconfans_rsync"
 
 echo `date +'%Y-%m-%d %H:%M:%S'` > /tmp/iconfans_rsync_check
 
-if [ -f $run_file ] 
-then 
+if [ -f $run_file ]
+then
 	echo "\n\ticonfans rsync 已经在运行\n"
 	exit
 fi
@@ -13,8 +13,8 @@ touch $run_file
 
 echo `date +'%Y-%m-%d %H:%M:%S'` > /tmp/iconfans_rsync_run
 
-/usr/bin/rsync --bwlimit=200 --temp-dir=/tmp --partial -vzrtopg -e ssh iconfans:/var/www/iconfans/ /home/zhengkai/iconfans/
-/usr/bin/rsync --bwlimit=200 --temp-dir=/tmp --partial -vzrtopg -e ssh iconfans:/backup/ /home/zhengkai/iconfans_db/
+/usr/bin/rsync --bwlimit=200 --temp-dir=/tmp --partial -vzrtopg -e ssh iconfans:/var/www/iconfans/ ~/iconfans/
+/usr/bin/rsync --bwlimit=200 --temp-dir=/tmp --partial -vzrtopg -e ssh iconfans:/backup/ ~/iconfans_db/
 
 sleep 5
 
