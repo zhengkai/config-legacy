a2268579cccc2c44cedd6bc37c4db70950346425
Zheng Kai <zhengkai@gmail.com>
2012-02-06 11:22:17 +0800

    dump script

---
+++ script/db_dump_soulogic.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+bakname="/home/zhengkai/backup/soulogic/db_`date +%y%m%d_%H%M`.sql.gz"
+
+dir=`dirname $bakname`
+
+find $dir -mtime +35 -exec rm -f {} \;
+
+/usr/bin/mysqldump --add-drop-database --add-drop-table --add-locks --hex-blob --quick --databases soulogic soulogic_pda | gzip --best > $bakname
+
