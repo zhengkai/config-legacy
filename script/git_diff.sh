bd085ae83a7a527908806ac9b0cbc90d78990d01
Zheng Kai <zhengkai@gmail.com>
2012-06-13 13:48:12 +0800

    up

--- script/git_diff.sh
+++ script/git_diff.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-vimdiff "$2" "$5"
+
+#alias gd='git diff --no-ext-diff -b -w -z --color=never | ~/conf/script/git_diff.php | less'
+
+export LINES
+
+let height=$LINES-3
+
+count=`git diff --no-ext-diff -b -w -z --color=never | ~/conf/script/git_diff.php | head -n $height | wc -l`
+
+if [ $count -ge $height ]; then
+	git diff --no-ext-diff -b -w -z --color=never | ~/conf/script/git_diff.php | less -r
+else
+	git diff --no-ext-diff -b -w -z --color=never | ~/conf/script/git_diff.php
+fi
 
