e6f43feeb2037ffd1502f0936246b614be8730f8
Zheng Kai <zhengkai@gmail.com>
2012-06-27 17:23:47 +0800

    up

--- script/tmux_tilda.sh
+++ script/tmux_tilda.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
-/usr/bin/tmux -2 attach -t tilda|| /usr/bin/tmux -2 new -s tilda
+
+name='tilda'
+window_num=6
+tmux='/usr/bin/tmux'
+
+if [ not $($tmux has-session -t $name 2>/dev/null) ] ; then
+
+	$tmux -2 new-session -d -s $name
+
+	i=$window_num
+	while [ $i -gt 1 ]; do
+		let i=$i-1
+		$tmux new-window -t "$name:$i"
+	done
+
+	$tmux select-window -t "$name:0"
+fi
+
+$tmux -2 attach -t $name
 
