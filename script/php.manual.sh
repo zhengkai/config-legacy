2ca8391052aa4890cb90d8e2861b8b1af98fad78
Zheng Kai <zhengkai@gmail.com>
2012-07-29 01:10:33 +0800

    up

--- script/php.manual.sh
+++ script/php.manual.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-/usr/bin/rsync --bwlimit=100 -avzC --timeout=600 --delete --delete-after --include='distributions/*.exe' rsync.php.net::phpweb /www/manual/php
+/usr/bin/rsync --bwlimit=100 -avzC --timeout=600 --delete --delete-after \
+ --exclude='.git' \
+ --include='manual/en/' --include='manual/en/**' --exclude='manual/**' \
+ --exclude='distributions' \
+ rsync.php.net::phpweb /www/manual/php
+
