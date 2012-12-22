c2136a285053344b77e98ec087ac664d4ae5fc23
Zheng Kai <zhengkai@gmail.com>
2012-12-23 01:24:15 +0800

    timezone

--- aptget.sh
+++ aptget.sh
@@ -2,6 +2,8 @@
 dir=$(dirname `readlink -f $0`);
 list=(`cat "$dir/aptget"`);
 
+sudo sh -c 'echo "PRC" > /etc/timezone'
+sudo cp /usr/share/zoneinfo/PRC /etc/localtime
+
 sudo apt-get update
 sudo apt-get install -y ${list[@]}
-
