2112a0cd6823b24473ac9440162731833e532699
Zheng Kai <zhengkai@gmail.com>
2014-05-06 15:52:54 +0800

    remove CNNIC

--- script/update_ubuntu.sh
+++ script/update_ubuntu.sh
@@ -4,3 +4,5 @@ sudo /usr/bin/apt-get -y dist-upgrade
 sudo /usr/bin/apt-get -y autoremove
 sudo /usr/bin/apt-get -y autoclean
 
+sudo rm -f /etc/ssl/certs/CNNIC_ROOT.pem
+sudo rm -f /usr/share/ca-certificates/mozilla/CNNIC_ROOT.crt
