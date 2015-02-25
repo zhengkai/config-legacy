7ca05d12d29181c47bb994fcb2f7b69b9ee9264c
Zheng Kai <zhengkai@gmail.com>
2015-02-25 15:11:07 +0800

    up

--- script/update_all.sh
+++ script/update_all.sh
@@ -2,6 +2,18 @@
 
 dir=$(dirname `readlink -f $0`);
 
+if [ -f $HOME/.vim/vundle.vim ]; then
+	vim +BundleInstall +qall
+	cd $dir
+fi
+
+conf_git=`$HOME/conf/.git`
+if [ -d "$conf_git" ]; then
+	cd $conf_git
+	git pull --rebase
+	cd $dir
+fi
+
 if [ -d "/www/manual/php" ]; then
 	$dir/php.manual.sh 2>&1
 fi
