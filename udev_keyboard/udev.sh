3e78fcfdbae5a82b808554adc732761b42977630
Zheng Kai <zhengkai@gmail.com>
2015-04-22 18:11:41 +0800

    up

--- udev_keyboard/udev.sh
+++ udev_keyboard/udev.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo -u zhengkai /usr/bin/xmodmap -display :0 /home/zhengkai/conf/.Xmodmap >/dev/null 2>&1 &
+sudo -u zhengkai /usr/bin/xmodmap -display :0 /home/zhengkai/.Xmodmap >/dev/null 2>&1 &
