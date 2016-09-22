a259ee439d7d708c55f3e55affedfc387f427108
Zheng Kai <zhengkai@gmail.com>
2016-09-22 12:47:28 +0800

    up

--- script/git_gc.sh
+++ script/git_gc.sh
@@ -4,10 +4,15 @@ pathList=`locate '.git' | grep -e '\.git$'`
 
 for s in ${pathList[@]}
 do
+
+	if [ ! -d "$s" ]; then
+		continue
+	fi
+
 	echo
 	date --date='TZ="Asia/Shanghai" now' +'%Y-%m-%d %H:%M:%S'
 	echo $s
 	echo
-	cd $s
+	cd $s || continue
 	git gc
 done
