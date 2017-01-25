873b4c7e87db633ae104ae80d4acff106c7667ca
Zheng Kai <zhengkai@gmail.com>
2017-01-25 12:20:26 +0800

    up

--- aptget.sh
+++ aptget.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 dir=$(dirname `readlink -f $0`)
-list=(`cat "$dir/aptget"`)
 
 sudo chown -R zhengkai:zhengkai /home/zhengkai
 
+sudo cp "$dir/file/sudoers_nopassword" /etc/sudoers.d/nopassword
+
+mkdir -p ~/.tmp
 mkdir -p ~/.backup
 
 sudo apt-get update
@@ -11,20 +13,13 @@ sudo apt-get update
 sudo apt-get install -y vim
 sudo update-alternatives --set editor /usr/bin/vim.basic
 
-sudo apt-get install -y ${list[@]}
-
-sudo apt-get remove -y fonts-droid
-
 sudo sh -c 'echo "Asia/Shanghai" > /etc/timezone'
 sudo cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
-mkdir -p ~/.tmp
-mkdir -p ~/.backup
+list=(`cat "$dir/aptget"`)
+sudo apt-get install -y ${list[@]}
 
-bashcomp_dir='/etc/bash_completion.d'
-if [ -d $bashcomp_dir ]; then
-	sudo cp "$dir/file/pman" $bashcomp_dir
-fi
+sudo apt-get remove -y fonts-droid
 
 sudo easy_install3 -U pip
 
@@ -37,4 +32,4 @@ sudo adduser zhengkai www-data
 
 sudo chown -R zhengkai:zhengkai /home/zhengkai
 
-sed "s/[[:blank:]]*$//" aptget | sort | uniq | sponge aptget
+#sed "s/[[:blank:]]*$//" aptget | sort | uniq | sponge aptget
