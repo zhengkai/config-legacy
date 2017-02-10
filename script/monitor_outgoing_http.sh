922d6458631437bfda48938a184e2af9897fe6e5
Zheng Kai <zhengkai@gmail.com>
2017-02-10 22:20:51 +0800

    up

--- script/monitor_outgoing_http.sh
+++ script/monitor_outgoing_http.sh
@@ -2,6 +2,10 @@
 
 IF='eth0'
 
+if [ `hostname` == "Tesla" ]; then
+	IF='enp2s0'
+fi
+
 sudo tcpflow -p -c -i $IF port 80 2>/dev/null \
 	| grep --line-buffered '^Host: ' \
 	| stdbuf -o0 cut -b '7-' \
