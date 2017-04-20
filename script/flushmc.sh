09e03947ed4c09168a48aa46ecd84b5e72ef91b7
Zheng Kai <zhengkai@gmail.com>
2017-04-20 17:56:19 +0800

    flushmc 并发

--- script/flushmc.sh
+++ script/flushmc.sh
@@ -5,22 +5,26 @@
 LIST=(`pgrep memcached`)
 
 for PID in "${LIST[@]}"; do
-
-	SEARCH=(`sudo netstat -nltp | grep $PID | grep -Po '([^\ ]*):\d+'`)
-	if [ -z "$SEARCH" ]; then
-		continue
-	fi
-	for ADDR in "${SEARCH[@]}"; do
-
-		HOST=${ADDR%:*}
-		PORT=${ADDR##*:}
-
-		MD_RETURN=`echo 'flush_all' | nc $HOST $PORT`
-		RESULT='OK'
-		if [ ${MD_RETURN//[[:space:]]/} != $RESULT ]; then
-			RESULT='FAIL'
+	{
+		SEARCH=(`sudo netstat -nltp | grep $PID | grep -Po '([^\ ]*):\d+'`)
+		if [ -z "$SEARCH" ]; then
+			continue
 		fi
+		for ADDR in "${SEARCH[@]}"; do
 
-		printf '%4s pid = %-6s addr = %-10s port = %s\n' $RESULT $PID',' $HOST',' $PORT
-	done
+			HOST=${ADDR%:*}
+			PORT=${ADDR##*:}
+
+			MD_RETURN=`echo 'flush_all' | nc $HOST $PORT`
+			RESULT='OK'
+			if [ ${MD_RETURN//[[:space:]]/} != $RESULT ]; then
+				RESULT='FAIL'
+			fi
+
+			printf '%4s pid = %-6s addr = %-10s port = %s\n' $RESULT $PID',' $HOST',' $PORT
+
+		done
+	} &
 done
+
+wait
