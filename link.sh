fd0aaee389b7ad81cb25600262083a14fa85e799
Zheng Kai <zhengkai@gmail.com>
2017-05-15 11:26:40 +0800

    link.sh

--- link.sh
+++ link.sh
@@ -1,11 +1,23 @@
-#!/bin/bash
+#!/bin/bash -ex
 dir=$(dirname `readlink -f $0`);
 list=(`cat "$dir/filelist"`);
 
 echo $dir/list
 
-for file in ${list[@]}
-do
+for file in ${list[@]}; do
+	if [ -e ~/$file ]; then
+		continue;
+	fi
 	ln -s -f $dir/$file ~/$file
 done
 
+cpfile=(
+	'.my.cnf'
+)
+
+for file in ${cpfile[@]}; do
+	if [ -e ~/$file ]; then
+		continue;
+	fi
+	cp $dir/$file ~/$file
+done
