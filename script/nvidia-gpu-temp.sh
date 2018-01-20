2086bf54fd7559ae6a4d772defddeb3e1a70f417
Zheng Kai <zhengkai@gmail.com>
2018-01-20 09:34:13 +0800

    up

--- script/nvidia-gpu-temp.sh
+++ script/nvidia-gpu-temp.sh
@@ -2,7 +2,7 @@
 
 T=`nvidia-smi -q --display=TEMPERATURE | grep 'GPU Current Temp' | cut -d':' -f2 | grep -o -P '\d+'`
 
-if [ "$T" -gt 80 ]; then
+if [ "$T" -gt 80 ] || [ "$T" -lt 20 ]; then
 	~/hide/mailgun/gpu-alert.sh "$T"
 	sudo shutdown -P 0
 fi
