05023270deadbae443ea9106be480bc3dd43ced8
Zheng Kai <zhengkai@gmail.com>
2018-06-04 10:24:54 +0800

    vim

---
+++ vim.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+VIM_DIR="$HOME/.vim"
+
+if [ -e "$VIM_DIR" ]; then
+	echo "$VIM_DIR ready been installed"
+	exit
+fi
+
+git clone git@github.com:zhengkai/vimrc.git "$VIM_DIR"
+
+mkdir "$HOME/.tmp"
+cd "$VIM_DIR"
+git submodule update --init --recursive
+vim +PlugInstall +qall
