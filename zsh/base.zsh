da8a79b49f75f3d08c91f7ba4f30e4a064fa5494
Zheng Kai <zhengkai@gmail.com>
2018-06-05 08:05:40 +0800

    zsh timer

--- zsh/base.zsh
+++ zsh/base.zsh
@@ -10,6 +10,7 @@ source $HOME/conf/zsh/prompt.zsh
 source $HOME/conf/zsh/vi.zsh
 source $HOME/conf/zsh/export.zsh
 source $HOME/conf/zsh/alias.zsh
+source $HOME/conf/zsh/timer.zsh
 
 HISTFILE=$HOME/conf/zsh/history.txt
 HISTSIZE=1000
