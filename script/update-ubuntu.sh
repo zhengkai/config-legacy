4f1f17d175ce1141e3c662e201c858dc9a27c1b7
Zheng Kai <zhengkai@gmail.com>
2018-08-17 19:21:44 +0800

    up

--- script/update-ubuntu.sh
+++ script/update-ubuntu.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-export DEBIAN_FRONTEND=noninteractive
-sudo /usr/bin/apt-get update
-sudo /usr/bin/apt-get -y -q dist-upgrade
-sudo /usr/bin/apt-get -y -q autoremove
-sudo /usr/bin/apt-get -y -q autoclean
+sudo DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -y -q update
+sudo DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -y -q -o Dpkg::Options::="--force-confnew" dist-upgrade
+sudo DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -y -q autoremove
+sudo DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -y -q autoclean
 
-sudo rm -f /etc/ssl/certs/CNNIC_ROOT.pem
-sudo rm -f /usr/share/ca-certificates/mozilla/CNNIC_ROOT.crt
+sudo rm -f /etc/ssl/certs/CNNIC_ROOT.pem || :
+sudo rm -f /usr/share/ca-certificates/mozilla/CNNIC_ROOT.crt || :
