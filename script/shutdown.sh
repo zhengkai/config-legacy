b23706147b05d9db0fd39020d1ce645d889cd664
Zheng Kai <zhengkai@gmail.com>
2018-08-20 10:52:14 +0800

    sdnow

---
+++ script/shutdown.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -z "$DISPLAY" ]; then
+	>&2 echo only run in X11
+	exit 1
+fi
+
+cd $(dirname `readlink -f $0`)
+
+echo apt update ...
+nohup ./update-ubuntu.sh 2>/dev/null
+
+echo shutdown
+sudo nuhup shutdown -P 0 2>/dev/null
