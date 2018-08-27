431635e7398eac4aa40c458a6abb5bad30071427
Zheng Kai <zhengkai@gmail.com>
2018-08-27 17:38:57 +0800

    up

--- script/all-domain-ssl.sh
+++ script/all-domain-ssl.sh
@@ -20,15 +20,18 @@ TIME=$((TIME+86400*15)) # 过期时间 15 天以内
 DOMAIN=($DOMAIN)
 for D in "${DOMAIN[@]}"
 do
-	echo domain $D
-	T=`./check-ssl-expire.sh "$D"`
+	echo -n domain $D ...
+	T=`./check-ssl-expire.sh "$D" 443 1`
 	if [ "$T" -lt $TIME ]; then
 		TITLE="SSL for $D expired"
 		CONTENT=`TZ="Asia/Shanghai" date -d "@$T" +'%Y-%m-%d %H:%M:%S'`
 		CONTENT="expired in $CONTENT // ~/conf/script/all-domain-ssl.sh"
+		echo fail
 		>&2 echo $TITLE
 		>&2 echo $CONTENT
 		~/hide/mailgun/alert.sh "$TITLE" "$CONTENT"
 		exit 1
+	else
+		echo ok
 	fi
 done
--- script/check-ssl-expire.sh
+++ script/check-ssl-expire.sh
@@ -2,15 +2,14 @@
 
 DOMAIN="${1:-soulogic.com}"
 PORT="${2:-443}"
-DATE="${3}"
 
-TIME=`echo | openssl s_client -showcerts -servername "$DOMAIN" -connect "${DOMAIN}:443" 2>/dev/null | openssl x509 -noout -dates | grep notAfter | cut -d '=' -f 2`
+TIME=`echo | timeout 10 openssl s_client -showcerts -servername "$DOMAIN" -connect "${DOMAIN}:${PORT}" 2>/dev/null | openssl x509 -noout -dates | grep notAfter | cut -d '=' -f 2`
 
 if [ -z "$TIME" ]; then
 	exit 1
 fi
 
-if [ -n "$DATE" ]; then
+if [ -z "$3" ]; then
 	echo domain: $DOMAIN
 	echo "  port: $PORT"
 	echo expire: `TZ='Asia/Shanghai' date '+%Y-%m-%d %H:%M:%S' -d "$TIME"`
