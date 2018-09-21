20d444b370e800faedbdb46c52eb6cb9e4182392
Zheng Kai <zhengkai@gmail.com>
2018-09-21 14:44:56 +0800

    tmux

--- script/tmux-tilda.sh
+++ script/tmux-tilda.sh
@@ -5,19 +5,19 @@ window_num=6
 tmux='/usr/bin/tmux'
 
 has_session=true
-$tmux has-session -t $name 2>/dev/null || has_sesion=false
+"$tmux" has-session -t $name 2>/dev/null || has_sesion=false
 
 if [ "$has_sesion" = false ]; then
 
-	$tmux -2 new-session -d -s $name
+	"$tmux" -2 new-session -d -s "$name"
 
 	i=$window_num
 	while [ $i -gt 1 ]; do
 		let i=$i-1
-		$tmux new-window -t "$name:$i"
+		"$tmux" new-window -t "$name:$i"
 	done
 
-	$tmux select-window -t "$name:0"
+	"$tmux" select-window -t "$name:0"
 fi
 
-$tmux -2 attach -t $name
+"$tmux" -2 attach -t "$name"
