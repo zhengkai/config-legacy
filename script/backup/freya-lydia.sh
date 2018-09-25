7a8c19cc4957c62a42937c3a5c4ad2d39dcdc0c4
Zheng Kai <zhengkai@gmail.com>
2018-09-25 10:15:47 +0800

    up

--- script/backup/freya-lydia.sh
+++ script/backup/freya-lydia.sh
@@ -7,11 +7,10 @@ flock -x -n 200 || exit 1
 source_path="freya:/log/"
 target_path="/backup/freya/log"
 
-echo start rsync
 rsync --temp-dir=/tmp --min-size=10 --partial -vzrtopg -e ssh "$source_path" "$target_path"
 
 ssh freya "find /log -type f -ctime +10        -delete"
-ssh freya "find /log -type d -ctime +10 -empty -delete"
+ssh freya "find /log -type d -ctime +1 -empty -delete"
 
 source_path="freya:/backup/Freya/"
 target_path="/backup/freya/db"
@@ -19,6 +18,6 @@ target_path="/backup/freya/db"
 rsync --temp-dir=/tmp --min-size=10 --partial -vzrtopg -e ssh "$source_path" "$target_path"
 
 ssh freya "find /backup -type f -ctime +10        -delete"
-ssh freya "find /backup -type d -ctime +10 -empty -delete"
+ssh freya "find /backup -type d -ctime +1  -empty -delete"
 
 ) 200>/tmp/rsync-freya-tesla.lock
