80556fdd1deb0c2ce7e46dfdb30ce16f798ac8ae
Zheng Kai <zhengkai@gmail.com>
2018-11-05 17:05:58 +0800

    up

---
+++ greasemonkey/test.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Test Only
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @updateURL    https://github.com/zhengkai/config/raw/master/greasemonkey/test.user.js
+// @author       Zheng Kai
+// @match        https://pig.9farm.com/
+// @grant        none
+// ==/UserScript==
+
+(() => {
+
+	'use strict';
+
+	console.log('yes pig', 'pig');
+
+})();
