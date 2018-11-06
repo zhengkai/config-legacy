4210aeec46f218101f7d6da2d331494979ba8c28
Zheng Kai <zhengkai@gmail.com>
2018-11-06 08:23:45 +0800

    up

--- greasemonkey/nga.user.js
+++ greasemonkey/nga.user.js
@@ -10,24 +10,34 @@
 
 (() => {
 
-	document.getElementById('mainmenu').style.margin = '0';
-	document.getElementById('toptopics').style.display = 'none';
-	document.getElementById('bbs_ads9_add').style.display = 'none';
+	try {
+		document.getElementById('mainmenu').style.margin = '0';
+		document.getElementById('toptopics').style.display = 'none';
+		document.getElementById('bbs_ads9_add').style.display = 'none';
+		document.getElementById('custombg').style.display = 'none';
+	} catch(x) {
+	}
 
 	const clearAD = () => {
-		const list = document.body.querySelectorAll('img');
-		for (const img of list) {
-			if (img.src.match('ngabbs/nga_classic/admark\.png')) {
-				img.parentNode.parentNode.remove());
-				continue;
+		try {
+			const list = document.body.querySelectorAll('img');
+			for (const img of list) {
+				if (img.src.match('ngabbs/nga_classic/admark\.png')) {
+					img.parentNode.parentNode.remove();
+					continue;
+				}
 			}
+		} catch(x) {
 		}
 	}
 
 	clearAD();
-	window.setTimeout(() => {
-		__COOKIE.setMiscCookieInSecond('pv_count_for_insad', -300);
-		clearAD();
-	}, 3000);
+	__COOKIE.setMiscCookieInSecond('pv_count_for_insad', -300);
+
+	for (let i = 0; i < 10; i++) {
+		window.setTimeout(() => {
+			clearAD();
+		}, i * 1000);
+	}
 
 })();
