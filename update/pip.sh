f59d39cfde63f5c19bf03f5301f4ff1606604ea7
Zheng Kai <zhengkai@gmail.com>
2019-09-24 15:00:42 +0800

    up

--- update/pip.sh
+++ update/pip.sh
@@ -4,6 +4,7 @@ sudo -H pip3 install --upgrade httpie
 sudo -H pip3 install --upgrade qrcode
 sudo -H pip3 install --upgrade tldr
 sudo -H pip3 install --upgrade thefuck
+sudo -H pip3 install --upgrade youtube-dl
 
 pip3 freeze --local \
 	| \grep -v '^\-e' \
