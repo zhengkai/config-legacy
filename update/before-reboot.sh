65a21043cedeee35fc68a31f6bade00b922482cb
Zheng Kai <zhengkai@gmail.com>
2019-10-21 16:11:33 +0800

    up

--- update/before-reboot.sh
+++ update/before-reboot.sh
@@ -11,10 +11,7 @@ if [ -e "$NODE" ]; then
 	"$NODE"
 fi
 
-sudo killall /usr/local/bin/ss-local
-sudo killall /usr/local/bin/ss-redir
-sudo killall /usr/local/bin/ss-server
 SS="${HOME}/build/shadowsocks/run.sh"
 if [ -e "$SS" ]; then
-	"$SS"
+	"$SS" kill
 fi
