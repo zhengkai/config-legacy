8007d4984f9d07fc65055dbe45368e3771afcbf1
Zheng Kai <zhengkai@gmail.com>
2020-02-06 13:00:36 +0800

    up

--- script/create-swap.sh
+++ script/create-swap.sh
@@ -8,4 +8,8 @@ fi
 mkdir /opt/images/
 rm -rf /opt/images/swap
 dd if=/dev/zero of=/opt/images/swap bs=1024 count=2048000
+# chmod 600 /opt/images/swap
+mkswap /opt/images/swap
 swapon /opt/images/swap
+
+free
--- script/git-hash.sh
+++ script/git-hash.sh
@@ -4,12 +4,18 @@ HASH=`git rev-parse --short HEAD 2>/dev/null || :`
 if [ -z "$HASH" ]; then
 	exit 1
 fi
-ST=`git status -s -u`
+
+TAG=`git describe --exact-match "$HASH" 2>/dev/null | head -n 1`
+if [ -n "$TAG" ]; then
+	HASH="$TAG"
+fi
+
+ST=`git status -s -u | head -n 1`
 if [ -n "$ST" ]; then
 	HASH="${HASH}-dirty"
 fi
 
-BRANCH=`git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1/"`
+BRANCH=`git branch --no-color 2>/dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1/"`
 if [[ "$BRANCH" == *"("* ]]; then
 	BRANCH=''
 else
