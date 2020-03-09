67e3773cc60b76d24af4eb36a34ddd6162f8bea6
Zheng Kai <zhengkai@gmail.com>
2020-03-09 14:20:45 +0800

    up

--- update/daily.sh
+++ update/daily.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
-DIR=`readlink -f "$0"` && DIR=`dirname "$DIR"` && cd "$DIR" || exit 1
+DIR=$(readlink -f "$0") && DIR=$(dirname "$DIR") && cd "$DIR" || exit 1
 
 sudo chown -R zhengkai:zhengkai /home/zhengkai &
 
-./npm.sh &
-./pip.sh &
+./npm.sh || : &
+./pip.sh || : &
 ./ubuntu.sh &
 
 CHN="$HOME/hide/redir/chn/update.sh"
