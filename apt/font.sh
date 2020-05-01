b53f57b768ec7fe591bfe2bc123f9b1ae2048520
Zheng Kai <zhengkai@gmail.com>
2020-05-01 15:18:41 +0800

    font

--- apt/font.sh
+++ apt/font.sh
@@ -1,12 +1,19 @@
-#! /bin/bash -ex
+#! /bin/bash -e
 
 TMP_FILE='/tmp/JetBrainsMono.zip'
 TMP_DIR='/tmp/JetBrainsMono'
 
-wget 'https://download.jetbrains.com/fonts/JetBrainsMono-1.0.3.zip' -O "$TMP_FILE"
+TARGET="/usr/local/share/fonts"
+if [ -f "${TARGET}/JetBrainsMono-Regular.ttf" ]; then
+	echo
+	echo font JetBrains Mono installed, exit
+	echo
+	exit
+fi
+
+curl 'https://download.jetbrains.com/fonts/JetBrainsMono-1.0.3.zip' -o "$TMP_FILE"
 
 mkdir -p "$TMP_DIR"
 cd "$TMP_DIR"
-unzip -j "$TMP_FILE" -d "$TMP_DIR"
-mkdir -p ~/.fonts
-cp JetBrainsMono*.ttf ~/.fonts
+unzip -o -j "$TMP_FILE" -d "$TMP_DIR"
+sudo cp JetBrainsMono*.ttf "$TARGET"
